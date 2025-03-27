fcb8b725a150ec24920a957b6edf21918898db3d
ed-matheus <edsonmatheus02@hotmail.com>
2025-03-27 15:12:46 -0300

    resgatando dados do "data.json" e exibindo cards dinâmicamente.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,59 @@
 import Image from "next/image";
-import ExtensionCard from "./components/ExtensionCard";
+import data from "@/data.json";
+
+// Components
+import ExtensionCard from "../components/ExtensionCard";
 
 export default function Home() {
-  return (
-    <main 
-      className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-5 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-b from-[#EBF2FC] to-[#EEF8F9]"
-    >
-      <header className="bg-neutral-0 rounded-xl p-3.5 mb-10 w-full flex items-center justify-between shadow">
-        <Image src={'/images/logo.svg'} alt="logo" width={200} height={200} />
-        <Image 
-          src={'/images/icon-moon.svg'} alt="color mode changer" 
-          width={60} 
-          height={60} 
-          className="bg-neutral-100 p-4 rounded-xl" 
-        />
-      </header>
+	// Resgatando dados do arquivo JSON local (data.json)
+	const extensionData = data;
 
-      <div className="w-full text-center mb-10">
-        <h1 className="font-bold text-neutral-900">Extensions List</h1>
-        <div className="flex justify-around">
-          <button type="button" className="bg-neutral-0 rounded-3xl py-4 px-6 shadow">All</button>
-          <button type="button" className="bg-neutral-0 rounded-3xl py-4 px-6 shadow">Active</button>
-          <button type="button" className="bg-neutral-0 rounded-3xl py-4 px-6 shadow">Inactive</button>
-        </div>
-      </div>
+	return (
+		<main className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-5 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-b from-[#EBF2FC] to-[#EEF8F9]">
+			<header className="bg-neutral-0 rounded-xl p-3.5 mb-10 w-full flex items-center justify-between shadow">
+				<Image src={"/images/logo.svg"} alt="logo" width={200} height={200} />
+				<Image
+					src={"/images/icon-moon.svg"}
+					alt="color mode changer"
+					width={60}
+					height={60}
+					className="bg-neutral-100 p-4 rounded-xl"
+				/>
+			</header>
 
-      <ExtensionCard />
-    </main>
-  );
+			<div className="w-full text-center mb-10">
+				<h1 className="font-bold text-neutral-900">Extensions List</h1>
+				<div className="flex justify-around">
+					<button
+						type="button"
+						className="bg-neutral-0 rounded-3xl py-4 px-6 shadow"
+					>
+						All
+					</button>
+					<button
+						type="button"
+						className="bg-neutral-0 rounded-3xl py-4 px-6 shadow"
+					>
+						Active
+					</button>
+					<button
+						type="button"
+						className="bg-neutral-0 rounded-3xl py-4 px-6 shadow"
+					>
+						Inactive
+					</button>
+				</div>
+			</div>
+
+			{extensionData.map((extension) => (
+				<ExtensionCard
+					key={extension.id}
+					name={extension.name}
+					description={extension.description}
+					icon={extension.logo}
+          isActive={extension.isActive}
+				/>
+			))}
+		</main>
+	);
 }
--- src/components/ExtensionCard.tsx
+++ src/components/ExtensionCard.tsx
@@ -4,23 +4,32 @@ import { useState } from "react";
 
 import Image from "next/image";
 
-const ExtensionCard = () => {
+type ExtensionCardProps = {
+	name: string;
+	description: string;
+	icon: string;
+	isActive: boolean;
+};
+
+const ExtensionCard: React.FC<ExtensionCardProps> = ({ name, description, icon, isActive }) => {
 	const [isOn, setIsOn] = useState(false);
 
+	// console.log(`${name} | ${description} | ${icon} | ${isActive}`);
+
 	return (
-		<div className="p-5 bg-neutral-0 shadow rounded-2xl w-full">
+		<div className="p-5 bg-neutral-0 shadow rounded-2xl w-full mb-4">
 			<div className="flex items-start">
 				<Image
-					src={"/images/logo-devlens.svg"}
+					src={icon}
 					alt="extension logo"
 					width={80}
 					height={80}
 				/>
 
 				<div className="ms-3.5">
-					<h2 className="font-bold text-2xl mb-2">DevLens</h2>
+					<h2 className="font-bold text-2xl mb-2">{name}</h2>
 					<span className="text-neutral-500">
-						Quickly inspect page layouts and visualize element boundaries.
+						{description}
 					</span>
 				</div>
 			</div>
@@ -40,9 +49,11 @@ const ExtensionCard = () => {
 						checked={isOn}
 						onChange={() => setIsOn(!isOn)}
 					/>
-          <div className="w-11 h-6 bg-gray-300 peer-checked:bg-red-700 rounded-full relative transition">
-            <div className={`absolute left-1 top-1 w-4 h-4 bg-white rounded-full transition ${isOn ? "translate-x-5" : ""}`} />
-          </div>
+					<div className="w-11 h-6 bg-gray-300 peer-checked:bg-red-700 rounded-full relative transition">
+						<div
+							className={`absolute left-1 top-1 w-4 h-4 bg-white rounded-full transition ${isOn ? "translate-x-5" : ""}`}
+						/>
+					</div>
 				</label>
 			</div>
 		</div>
