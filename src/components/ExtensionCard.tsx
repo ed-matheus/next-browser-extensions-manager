da50bda3d5956791d7ec494e86092e01102a0b3d
ed-matheus <edsonmatheus02@hotmail.com>
2025-04-03 14:44:52 -0300

    a little change in the card component.

--- src/components/ExtensionCard.tsx
+++ src/components/ExtensionCard.tsx
@@ -24,7 +24,7 @@ const ExtensionCard: React.FC<ExtensionCardProps> = ({
 	// console.log(`${name} | ${description} | ${icon} | ${isActive}`);
 
 	return (
-		<div className={`p-5 ${isDarkMode ? 'bg-neutral-800' : ''} shadow rounded-2xl w-full mb-4`}>
+		<div className={`p-5 ${isDarkMode ? 'bg-neutral-800' : 'bg-neutral-0'} shadow rounded-2xl w-full mb-4`}>
 			<div className="flex items-start">
 				<Image src={icon} alt="extension logo" width={80} height={80} />
 
