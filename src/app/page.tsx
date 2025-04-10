1b6d7b042210b1c9a65a7ab375270846f10eee63
ed-matheus <edsonmatheus02@hotmail.com>
2025-04-10 14:48:08 -0300

    a little change, so the build can run with success

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,122 +8,130 @@ import { useEffect, useState } from "react";
 import ExtensionCard from "../components/ExtensionCard";
 import FilterButton from "@/components/FilterButton";
 
+interface Extension {
+  id: number;
+  name: string;
+  description: string;
+  logo: string;
+  isActive: boolean;
+}
+
 export default function Home() {
-	// Getting data from data.json file
-	const extensionData = data;
+  // Getting data from data.json file
+  const extensionData = data;
 
-	// States
-	const [activeFilter, setActiveFilter] = useState("all");
-	const [filteredExtensions, setFilteredExtensions] = useState(extensionData);
-	const [isDarkMode, setIsDarkMode] = useState(false);
+  // States
+  const [activeFilter, setActiveFilter] = useState<"all" | "active" | "inactive">("all");
+  const [filteredExtensions, setFilteredExtensions] = useState(extensionData);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [colorMode, setColorMode] = useState<"dark" | "light" | null>(null);
 
-	useEffect(() => {
-		const theme = localStorage.getItem("theme");
-		if (theme === "dark") {
-			setIsDarkMode(true);
-			document.documentElement.classList.add("dark");
-		} else {
-			setIsDarkMode(false);
-			document.documentElement.classList.remove("dark");
-		}
-	}, []);
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    setColorMode(storedTheme as "dark" | "light" | null);
+    if (storedTheme === "dark") {
+      setIsDarkMode(true);
+      document.documentElement.classList.add("dark");
+    } else {
+      setIsDarkMode(false);
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
 
-	const toggleDarkMode = () => {
-		setIsDarkMode((prevMode) => {
-			const newMode = !prevMode;
-			document.documentElement.classList.toggle("dark", newMode);
-			localStorage.setItem("theme", newMode ? "dark" : "light");
-			return newMode;
-		});
-	};
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevMode) => {
+      const newMode = !prevMode;
+      document.documentElement.classList.toggle("dark", newMode);
+      localStorage.setItem("theme", newMode ? "dark" : "light");
+      setColorMode(newMode ? 'dark' : 'light');
+      return newMode;
+    });
+  };
 
-	const colorMode = localStorage.getItem("theme")
-	// console.log(colorMode)
+  const handleExtensionsFilter = (filter: "all" | "active" | "inactive") => {
+    setActiveFilter(filter);
 
-	const handleExtensionsFilter = (filter: "all" | "active" | "inactive") => {
-		setActiveFilter(filter);
+    // Filtering Data
+    switch (filter) {
+      case "active":
+        return setFilteredExtensions(
+          extensionData.filter((extension) => extension.isActive === true),
+        );
+      case "inactive":
+        return setFilteredExtensions(
+          extensionData.filter((extension) => extension.isActive === false),
+        );
+      default:
+        return setFilteredExtensions(extensionData);
+    }
+  };
 
-		// Filtering Data
-		switch (filter) {
-			case "active":
-				return setFilteredExtensions(
-					extensionData.filter((extension) => extension.isActive === true),
-				);
-			case "inactive":
-				return setFilteredExtensions(
-					extensionData.filter((extension) => extension.isActive === false),
-				);
-			default:
-				return setFilteredExtensions(extensionData);
-		}
-	};
+  const handleRemoveExtension = (name: string) => {
+    setFilteredExtensions((prev) =>
+      prev.filter((extension) => extension.name !== name)
+    )
+  }
 
-	const handleRemoveExtension = (name: string) => {
-		setFilteredExtensions((prev) => 
-			prev.filter((extension) => extension.name !== name)
-		)
-	}
+  return (
+    <main className={`grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-5 pb-20 gap-16 sm:p-20 font-[faily-name:var(--font-geist-sans)] ${isDarkMode ? "bg-gradient-to-b from-[#040918] to-[#091540]" : "bg-gradient-to-b from-[#EBF2FC] to-[#EEF8F9]"}`}>
+      <header className={`${isDarkMode ? "bg-neutral-800" : "bg-neutral-0"} rounded-xl p-3.5 mb-10 w-full flex items-center justify-between shadow lg:w-[52rem] xl:w-[77rem]`}>
+        <Image src={"/images/logo.svg"} alt="logo" width={200} height={200} />
 
-	return (
-		<main className={`grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-5 pb-20 gap-16 sm:p-20 font-[faily-name:var(--font-geist-sans)] ${isDarkMode ? "bg-gradient-to-b from-[#040918] to-[#091540]" : "bg-gradient-to-b from-[#EBF2FC] to-[#EEF8F9]"}`}>
-			<header className={`${isDarkMode ? "bg-neutral-800" : "bg-neutral-0"} rounded-xl p-3.5 mb-10 w-full flex items-center justify-between shadow lg:w-[52rem] xl:w-[77rem]`}>
-				<Image src={"/images/logo.svg"} alt="logo" width={200} height={200} />
+        {/* Theme changer button */}
+        <button
+          type="button"
+          className="cursor-pointer"
+          onClick={toggleDarkMode}
+        >
+          <Image
+            src={isDarkMode ? "/images/icon-sun.svg" : "/images/icon-moon.svg"}
+            alt={isDarkMode ? "Dark mode" : "Light mode"}
+            className={`${isDarkMode ? "bg-neutral-700 hover:bg-neutral-600" : "bg-neutral-100 hover:bg-neutral-300"} p-4 rounded-xl w-[60] h-[60]`}
+            width={60}
+            height={60}
+          />
+        </button>
+      </header>
 
-				{/* Theme changer button */}
-				<button
-					type="button"
-					className="cursor-pointer"
-					onClick={toggleDarkMode}
-				>
-					<Image
-						src={isDarkMode ? "/images/icon-sun.svg" : "/images/icon-moon.svg"}
-						alt={isDarkMode ? "Dark mode" : "Light mode"}
-						className={`${isDarkMode ? "bg-neutral-700 hover:bg-neutral-600" : "bg-neutral-100 hover:bg-neutral-300"} p-4 rounded-xl w-[60] h-[60]`}
-						width={60}
-						height={60}
-					/>
-				</button>
-			</header>
+      {/* Filtro */}
+      <div className="w-full text-center mb-10 lg:flex lg:justify-between lg:w-[52rem] xl:w-[77rem]">
+        <h1 className={`${isDarkMode ? "text-neutral-0" : "text-neutral-900"} font-bold`}>Extensions List</h1>
+        <div className="flex justify-around lg:gap-5 lg:py-3 xl:gap-5 xl:py-4">
+          <FilterButton
+            text="All"
+            isActive={activeFilter === "all"}
+            onClick={() => handleExtensionsFilter("all")}
+            isDarkMode={colorMode === 'dark'}
+          />
+          <FilterButton
+            text="Active"
+            isActive={activeFilter === "active"}
+            onClick={() => handleExtensionsFilter("active")}
+            isDarkMode={colorMode === 'dark'}
+          />
+          <FilterButton
+            text="Inactive"
+            isActive={activeFilter === "inactive"}
+            onClick={() => handleExtensionsFilter("inactive")}
+            isDarkMode={colorMode === 'dark'}
+          />
+        </div>
+      </div>
 
-			{/* Filtro */}
-			<div className="w-full text-center mb-10 lg:flex lg:justify-between lg:w-[52rem] xl:w-[77rem]">
-				<h1 className={`${isDarkMode ? "text-neutral-0" : "text-neutral-900"} font-bold`}>Extensions List</h1>
-				<div className="flex justify-around lg:gap-5 lg:py-3 xl:gap-5 xl:py-4">
-					<FilterButton
-						text="All"
-						isActive={activeFilter === "all"}
-						onClick={() => handleExtensionsFilter("all")}
-						isDarkMode={colorMode === 'dark' ? isDarkMode === true : false}
-					/>
-					<FilterButton
-						text="Active"
-						isActive={activeFilter === "active"}
-						onClick={() => handleExtensionsFilter("active")}
-						isDarkMode={colorMode === 'dark' ? isDarkMode === true : false}
-					/>
-					<FilterButton
-						text="Inactive"
-						isActive={activeFilter === "inactive"}
-						onClick={() => handleExtensionsFilter("inactive")}
-						isDarkMode={colorMode === 'dark' ? isDarkMode === true : false}
-					/>
-				</div>
-			</div>
-
-			{/* Renderização dinâmica dos cards */}
-			<section className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 xl:w-[77rem]">
-				{filteredExtensions.map((extension) => (
-					<ExtensionCard
-						key={extension.id}
-						name={extension.name}
-						description={extension.description}
-						icon={extension.logo}
-						isActive={extension.isActive}
-						isDarkMode={colorMode === 'dark' ? isDarkMode === true : false}
-						onRemove={handleRemoveExtension}
-					/>
-				))}
-			</section>
-		</main>
-	);
+      {/* Renderização dinâmica dos cards */}
+      <section className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 xl:w-[77rem]">
+        {filteredExtensions.map((extension) => (
+          <ExtensionCard
+            key={extension.id}
+            name={extension.name}
+            description={extension.description}
+            icon={extension.logo}
+            isActive={extension.isActive}
+            isDarkMode={colorMode === 'dark'}
+            onRemove={handleRemoveExtension}
+          />
+        ))}
+      </section>
+    </main>
+  );
 }
